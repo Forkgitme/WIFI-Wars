b40ad0b30a1570d48c04a9a71265b021226cfa24
Jeswer9 <negen_jewer@live.nl>
2015-02-24 15:09:31 +0100

    Naam map aanpassen

---
+++ Wi-Fi Wars/WifiWars/WifiWars/states/PlayingState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+class PlayingState : IGameLoopObject
+{
+    public PlayingState(ContentManager Content)
+    {
+    }
+
+    public void HandleInput(InputHelper inputHelper)
+    {
+
+    }
+
+    public void Update(GameTime gameTime)
+    {
+
+    }
+
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+}
