56f633c3d9ffe4c2c081ebfe74c2275c75900237
Kalsir <sylkegosen@hotmail.com>
2015-02-25 10:55:42 +0100

    New version

---
+++ Wi-Fi Wars2/TickTick/TickTick5/gameobjects/Tower.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+    class Tower : SpriteGameObject
+    {
+        bool placed;
+        public Tower(int layer = 0, string id = "tower") : base("Sprites/tower", layer, id) 
+        {
+            this.Position = new Vector2(-1000, -1000);
+        }
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            Level level = this.parent as Level;
+            SpriteGameObject background = level.Find("background") as SpriteGameObject;
+            if (!placed && inputHelper.MousePosition.X > 0 && inputHelper.MousePosition.Y > 0 && inputHelper.MousePosition.Y < 825 && inputHelper.MousePosition.X < 1440)
+            {
+                this.position = inputHelper.MousePosition - this.Center;
+                if (background.Sprite.GetPixelColor((int)position.X + (int)Center.X, (int)position.Y + (int)Center.Y).G == 255)
+                {
+                    this.Sprite.SpriteColor = Color.White;
+                    if (inputHelper.MouseLeftButtonPressed())
+                    {
+                        this.placed = true;
+                        level.HoldingTower = false;
+                    }
+                }
+                else
+                    this.Sprite.SpriteColor = Color.Red;
+            }
+        }
+    }
---
+++ Wi-Fi Wars2/TickTick/TickTick5/level/LevelLoading.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+partial class Level : GameObjectList
+{
+    public void LoadLevel(string path)
+    {
+        List<string> textlines = new List<string>();
+        StreamReader fileReader = new StreamReader(path);
+        string line = fileReader.ReadLine();
+        while (line != null)
+        {
+            string[] obj = line.Split(' ');
+            Vector2 position = new Vector2(float.Parse(obj[1]), float.Parse(obj[2]));
+
+            switch (obj[0])
+            {
+                case "server":
+                    if (obj[3] != null)
+                    {
+                        this.Add(new Server(int.Parse(obj[3]), position, 1, "server" + obj[3]));
+                    }
+                    else throw new IOException("Server objects needs 4 values, name, coords and colorcode(1-3)");
+                    break;
+                case "home": break;
+                case "hideout": break;
+                case "antipirate": break;
+                default: throw new IOException("Unknown object type: " + obj[0]);
+            }
+
+            line = fileReader.ReadLine();
+        }
+        fileReader.Close();
+    }
+
+}
