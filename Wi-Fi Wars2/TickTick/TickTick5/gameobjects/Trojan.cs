8e32a4463c171890ffe2be856f6b6efebd48aa20
Robin Sikkens <robinsikkens@live.nl>
2015-03-17 13:19:01 +0100

    Tróhàn Hurseees

---
+++ Wi-Fi Wars2/TickTick/TickTick5/gameobjects/Trojan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+class Trojan : Virus
+{
+    bool hidden = true;
+    Server serv;
+
+    public Trojan(Vector2 spawnPosition, Server server, GameObjectList list, SpriteGameObject obj)
+        : base(spawnPosition, server, list ,obj)
+    {
+        this.Sprite = new SpriteSheet("Sprites/Pirateship", 0);
+        serv = server;
+    }
+
+    public override void Changes()
+    {
+        if (hidden)
+        {
+            hidden = false;
+            this.Sprite = new SpriteSheet("Sprites/Virus", 0);
+        }
+        else
+        {
+            delete = true;
+        }
+    }
+}
+
